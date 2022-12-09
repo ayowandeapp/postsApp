034b0fd94bad326ca2fb7c717d7a658fa431daa9
ayowandeapp <ayooluwa71@gmail.com>
2022-12-09 22:28:13 +0100

    Add author route
    add route for author resources

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'PostController@home');
 Route::post('/posts/store', 'PostController@store');
-Route::match(['get'],'/posts/{id}edit/',[
-    'uses'=>'PostController@edit'
-]);
+Route::match(['get'],'/posts/{id}/edit','PostController@edit');
+Route::patch('/posts/{id}/edit','PostController@update');
 Route::get('/posts/{id}', 'PostController@show');
-Route::resource('/posts', 'PostController');
+Route::delete('/posts/{id}','PostController@destroy');
+//Route::resource('/posts', 'PostController');
+
+Route::post('/author/store', 'AuthorController@store');
