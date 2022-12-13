808181ee1a8abc62df1d38a3c235bc070576197c
ayowandeapp <ayooluwa71@gmail.com>
2022-12-13 17:23:27 +0100

    Add Like resources
    added the like controller, migration and controller files

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Like;
+use App\Post;
+
+class LikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($post_id)
+    {
+        Like::create([
+            'post_id'=>$post_id]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'like'=> 'required']);
+        //if post exist in post table
+        $post = Post::where('id',$id)->first();
+        if(!$post){
+            return response()->json([
+                'message'=>'posts does not exist']);
+        }
+        $like = Like::where('post_id',$id);
+        if ($like->count() == 0) {
+            $this->create($id);
+        }
+        $like->update(['like'=>$request->like]);
+        return response()->json([
+            'message'=>'updated',
+            'data'=>$like->first()]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
